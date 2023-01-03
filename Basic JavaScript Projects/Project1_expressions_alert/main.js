aaca7ef2e975a9c1559230bc14c0dfc660804cf1
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-02 18:43:18 -0800

    pushing BAsic JavaScript project assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,10 @@
+var Zip = ("welcome to my Page"); //Defines Variable Zip naming it
+window.alert(Zip); 
+var Family = "The Pettys", Dad = "Dezden", Mom = "Alissa", Daughter = "Brynn", Son = "Kaden";//Defines Variable and gives values
+var Sent1 =("This is the beginning of the string"); //Defines variable and gives string value
+var Sent2 = ("and this is the end of the string"); // Defines Variable and gives string Value
+document.write(Sent1+Sent2);
+function My_First_Function() {        //Defining a Function and naming it
+    var str = "This is the button text!"; //Defining a Variable and giving it a string value
+    document.getElementById("Button_Text") .innerHTML = str; //puting the value of the variable into the HTML elementFromPoint // with the "Button_Text" id
+}
