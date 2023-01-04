e4c0660f521a1880807b4b0cf1c3e242fed0396b
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-03 23:18:13 -0800

    project 6

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/main.js
@@ -0,0 +1,27 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + "to ride";
+}
+
+function Voting_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough to vote" : "You can vote";
+    document.getElementById("Vote").innerHTML = Can_vote + "to vote";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + "manufactured in " + Erik.Vehicle_Year;
+}
