31541f67d7cce340a5c1fba0a80dad45ae782329
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-03 21:01:34 -0800

    Dictionary submission

--- Basic JavaScript Projects/Project4_Dictionaries/main.js
+++ Basic JavaScript Projects/Project4_Dictionaries/main.js
@@ -6,5 +6,6 @@ function my_Dictionary() { //creates Dictionary
         Age:5, //Because 5 is the value type “number,” we don’t have to place it within quotation marks.
         Sound:"Bark!" //sub set variables
     };
+    delete Animal.Sound; // removes Dictionary entry Animal.Sound
     document.getElementById("Dictionary").innerHTML = Animal.Sound;//The output of our code would be: Bark! Because this was associated with the key “Sound.”
 }
