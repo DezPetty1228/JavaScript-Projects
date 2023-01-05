51eb9d09642b3a98006d3ea5b7cb3ecf29e50d6c
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-04 16:34:00 -0800

    corrected Dictionary

--- Basic JavaScript Projects/Project4_Dictionaries/main.js
+++ Basic JavaScript Projects/Project4_Dictionaries/main.js
@@ -1,11 +1,9 @@
-function my_Dictionary() { //creates Dictionary
-    var Animal = { //Defines variables
-        Species:"Dog", //sub set variables
-        Color: "Black", //sub set variables
-        Breed:"Labrador" //sub set variables
-        Age:5, //Because 5 is the value type “number,” we don’t have to place it within quotation marks.
-        Sound:"Bark!" //sub set variables
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color: "Black",
+        Breed: "Pitbull",
+        Sound: "Bark!"
     };
-    delete Animal.Sound; // removes Dictionary entry Animal.Sound
-    document.getElementById("Dictionary").innerHTML = Animal.Sound;//The output of our code would be: Bark! Because this was associated with the key “Sound.”
+document.getElementById("dictionary").innerHTML = Animal.Breed;
 }
