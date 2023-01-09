4ede5cab22397f981bf5c884dfe2df049972a3f8
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-08 20:06:40 -0800

    project 7

--- Basic JavaScript Projects/Project7_scope_time_function/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/main.js
@@ -1,18 +1,14 @@
-var X = 10;
-function Add_numbers_1() {
-    document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-    document.write(X + 100);
-}
-Add_numbers_1();
-Add_numbers_2();
-
-if (1 < 2) {
-    document.write("The left number is smaller than the number on the right.");
-}
-function get_Date() {
-    if (new Date().getHours() < 18) {
-        document.getElementById("scope").innerHTML = "How are you today";
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
     }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
