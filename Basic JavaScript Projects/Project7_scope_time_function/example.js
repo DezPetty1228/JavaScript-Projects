5910854193b2a9c8eae351403bf358be033c7a9d
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-01-08 19:52:34 -0800

    project 7

---
+++ Basic JavaScript Projects/Project7_scope_time_function/example.js
@@ -0,0 +1,10 @@
+function age_Function() {
+    Age = document.getElementById.getElementById("Age").value;
+    if (Age = >= 18) {
+        vote = "you are old enough to vote!";
+    }
+    else {
+        "you are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
